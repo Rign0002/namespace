9eab0220081ac541d6aac3b2a484902b3b0a9bec
Justin <rign0002@algonquinlive.com>
2018-10-22 16:33:26 -0400

    wrote the code

--- namespace.js
+++ namespace.js
@@ -1,7 +1,29 @@
-var rign0002
-
 document.addEventListener("DOMContentLoaded", init);
 
-function init(){
-    
-}
+var RIGN0002 = {
+    init: function () {
+        var newDiv = document.createElement("div");
+        newDiv.className = "box";
+        newDiv.textContent = "RIGN0002";
+        var boxes = document.getElementById("boxes");
+        boxes.appendChild(newDiv);
+        
+        newDiv.addEventListener("click", function () {
+            newDiv.style.borderColor = "darkblue";
+            newDiv.style.backgroundColor = "aqua";
+        });
+
+        newDiv.addEventListener("mouseover", function () {
+
+            newDiv.classList.toggle("highlight");
+
+        });
+
+        newDiv.addEventListener("mouseout", function () {
+
+            newDiv.classList.toggle("highlight");
+            newDiv.removeAttribute('style');
+        });
+
+    }
+};
